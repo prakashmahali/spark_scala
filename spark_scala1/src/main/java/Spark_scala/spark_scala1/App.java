56a2ff40480056899b1f181dbfe0c578a0dbcd43
prakashmahali <prakash.mahali@gmail.com>
2018-12-14 22:29:09 -0600

    first commit

---
+++ spark_scala1/src/main/java/Spark_scala/spark_scala1/App.java
@@ -0,0 +1,13 @@
+package Spark_scala.spark_scala1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ spark_scala1/src/test/java/Spark_scala/spark_scala1/AppTest.java
@@ -0,0 +1,20 @@
+package Spark_scala.spark_scala1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
